0eed4b5d3240a01f529a78acf660c53b528fc074
YG Min <ygminceo@gmail.com>
2021-07-05 13:13:14 -0400

    ssr

--- packages/lib-frontend/src/usage/containers/UsageProvider/_internal/_initialize.ts
+++ packages/lib-frontend/src/usage/containers/UsageProvider/_internal/_initialize.ts
@@ -1,20 +1,16 @@
 import mixpanel from 'mixpanel-browser';
 import { config } from '@lib/common/core/utils/Config/Config';
-import { Platform } from '@lib/frontend/core/utils/Platform/Platform';
 import { UsageContextModel } from '@lib/frontend/usage/containers/UsageProvider/UsageProvider.model';
 
 const REACT_APP_MIXPANEL_TOKEN = config.get<string>('REACT_APP_MIXPANEL_TOKEN', '');
 
-export const _initialize = (): Promise<UsageContextModel | null> =>
+export const _initialize = async (): Promise<UsageContextModel | null> =>
   new Promise((resolve) => {
-    if (Platform.isWebApp) {
-      mixpanel.init(REACT_APP_MIXPANEL_TOKEN);
-      return resolve({
-        isReady: true,
-        identify: mixpanel.identify,
-        reset: mixpanel.reset,
-        track: (event) => mixpanel.track(`${event.object} ${event.action}`, event.params),
-      });
-    }
-    return resolve(null);
+    mixpanel.init(REACT_APP_MIXPANEL_TOKEN);
+    return resolve({
+      isReady: true,
+      identify: mixpanel.identify,
+      reset: mixpanel.reset,
+      track: (event) => mixpanel.track(`${event.object} ${event.action}`, event.params),
+    });
   });
