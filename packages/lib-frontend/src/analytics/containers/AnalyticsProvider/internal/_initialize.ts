09a5fa031e3b0bb7f7e7b25ac89d9d311e2fc493
YG Min <ygminceo@gmail.com>
2021-07-13 18:53:59 -0400

    analytics

--- packages/lib-frontend/src/analytics/containers/AnalyticsProvider/internal/_initialize.ts
+++ packages/lib-frontend/src/analytics/containers/AnalyticsProvider/internal/_initialize.ts
@@ -2,22 +2,19 @@ import { config } from '@lib/common/core/utils/Config/Config';
 import { analyticsMock } from '@lib/frontend/analytics/containers/AnalyticsProvider/AnalyticsProvider';
 import { AnalyticsContextModel } from '@lib/frontend/analytics/containers/AnalyticsProvider/AnalyticsProvider.model';
 import { Platform } from '@lib/frontend/core/utils/Platform/Platform';
-import { identify, init, reset, track } from 'mixpanel-browser';
+import mixpanel from 'mixpanel-browser';
 
 const REACT_APP_MIXPANEL_API_KEY = config.get<string>('REACT_APP_MIXPANEL_API_KEY', '');
 
-export const _initialize = async (): Promise<AnalyticsContextModel> =>
+export const _initialize = async (): Promise<AnalyticsContextModel | null> =>
   new Promise((resolve) => {
     if (Platform.isWeb) {
-      init(REACT_APP_MIXPANEL_API_KEY);
-      console.warn('******');
-      console.warn(REACT_APP_MIXPANEL_API_KEY);
-      console.warn('******');
+      mixpanel.init(REACT_APP_MIXPANEL_API_KEY);
       return resolve({
         isReady: true,
-        identify: (uid: string) => identify(uid),
-        reset: () => reset(),
-        track: (event) => track(`${event.object} ${event.action}`, event.params),
+        identify: (uid: string) => mixpanel.identify(uid),
+        reset: () => mixpanel.reset(),
+        track: (event) => mixpanel.track(`${event.object} ${event.action}`, event.params),
       });
     }
     return resolve(analyticsMock);
